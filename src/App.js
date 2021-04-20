feec4e70529a9e69b90d3f236b7908f5605b9991
snowquirk <snowquirk@gmail.com>
2021-04-20 13:40:10 +1000

    add header and links

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
+import Header from './components/Header/Header';
 import HomePage from './pages/Homepage/HomePage';
 import ShopPage from './pages/ShopPage/ShopPage';
 
 function App() {
   return (
     <div>
-      <Route exact path='/' component={HomePage} />
-      <Route exact path='/shop' component={ShopPage} />
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
